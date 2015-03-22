48f029a5059dae3f91008225dae2f2469f1e8ed1
Vexatos <Vexatos@users.noreply.github.com>
2015-03-22 19:41:49 +0100

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -3,7 +3,7 @@ var repoBackup;
 
 function initBackup() {
     repoBackup = [];
-    for(var i=0; i < repos.length; i++) {
+    for(var i = 0; i < repos.length; i++) {
         repoBackup.push(repos[i].cloneNode(true));
     }
 }
@@ -18,9 +18,9 @@ function search(event) {
     if(search.length < 2) {
         searchRepos = repoBackup;
     } else {
-        for(var i=0; i < repoBackup.length; i++) {
+        for(var i = 0; i < repoBackup.length; i++) {
             var part = repoBackup[i];
-            if(part && part.textContent && part.textContent.toLowerCase().indexOf(search) > 0) {
+            if(part && part.textContent && part.textContent.toLowerCase().indexOf(search) >= 0) {
                 searchRepos.push(part);
             }
         }
@@ -29,7 +29,7 @@ function search(event) {
         var part = repos[0];
         part.parentNode.removeChild(part);
     }
-    for(var i=0; i < searchRepos.length; i++) {
+    for(var i = 0; i < searchRepos.length; i++) {
         var part = searchRepos[i];
         parent.appendChild(part.cloneNode(true));
     }
