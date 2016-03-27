7ef7766a2598e56a24941ad405fa08fe9d5ce38a
PixelToast <andre.lipke@gmail.com>
2016-03-27 00:22:21 -0400

    Remove css overwriting

--- generate.lua
+++ generate.lua
@@ -110,76 +110,6 @@ local res,err=xpcall(function()
     ipack="/blob/master/immibis-compress/ipack.lua",
     dnsd="/blob/master/dns-server.lua",
   }
-
-  local css=[[
-body {
-  background-color:#101010;
-  color:#101010;
-  font-family:Arial, Helvetica, sans-serif
-}
-a:link {text-decoration:none;}
-a:visited {text-decoration:none;}
-a:hover {text-decoration:none;}
-a:active {text-decoration:none;}
-a {
-  font-weight:bold;
-  color:#101010;
-}
-h2 {
-  background-color:#F0F0F0;
-  color:#101010;
-}
-.bvc { left: -10px }
-.bevel, .content { border-width: 10px }
-.bevel, .content { border-color: #F0F0F0; border-style:solid; }
-
-.bvc {
-  margin: 15 15 15 15;
-  position: relative; `   margin-bottom: 0px;
-}
-
-.bvc .tr, .bvc .tl, .bvc .br, .bvc .bl { height: 0px; width: 100%; }
-.bvc .tr, .bvc .tl { border-top: 0; }
-.bvc .br, .bvc .bl { border-bottom: 0; }
-.bvc .tr, .bvc .br { border-right-color: transparent; }
-.bvc .tl, .bvc .bl { border-left-color: transparent; }
-.no_bevel { height: 0px; width: 100%; border-bottom: 0; }
-
-.content {
-  width: 100%;
-  border-top: 0;
-  border-bottom: 0;
-  color:#101010;
-  background-color:#F0F0F0;
-}
-
-.programs {
-  color:#101010;
-  background-color:#F0F0F0;
-  border-radius:5px;
-  padding:10px;
-}
-.title {
-  font-weight:bold;
-  text-align:center;
-  font-size:130%;
-  color:#101010;
-  background-color:#E0E0E0;
-  border-radius:5px;
-  text-align:center;
-  padding:5px;
-}
-h5 {
-  color:#F0F0F0;
-}
-table {
-  border:0px;
-  padding:10px;
-}
-td {
-  border:0px;
-}
-]]
   local html=[[
 <html>
 	<head>
@@ -233,12 +163,8 @@ td {
   </body>
 </html>
   ]]
-  css=css
   local file=assert(io.open((arg or "").."index.html","w"))
   file:write(html)
-  file:close()
-  local file=assert(io.open((arg or "").."style.css","w"))
-  file:write(css)
   file:close()
 end,debug.traceback)
 if not res then
